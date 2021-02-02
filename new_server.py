534d710be1bc49affcd8bbeb4e1c1e7112ac6263
sahiljain11 <sahilbot1@gmail.com>
2021-02-01 21:14:30 -0600

    Removed PIL

--- new_server.py
+++ new_server.py
@@ -7,7 +7,7 @@ import string
 from flask_session import Session
 from io import BytesIO
 import base64
-from PIL import Image
+#from PIL import Image
 
 app = Flask(__name__)
 app.secret_key = 'afisdosad90akfsdial1239jk'
@@ -158,22 +158,22 @@ def get_trajectory(trajectory_id):
 def save_trajectory():
   return 'sequence saved', 200
 
-@app.route('/api/save_frame', methods=['POST'])
-def save_frame():
-  resp = request.get_json()
-  
-  data = resp['screenshot'].split(',')[1]
-  w = resp['width']
-  h = resp['height']
-  key = resp['key']
-  count = resp['count']
-  im = Image.open(BytesIO(base64.b64decode(data)))
-  im = im.crop((0,0,w,h))
-
-  path = os.path.join(os.getcwd(), f'images/{key}_{count}.png')
-  print(path)
-  im.save(path)
-  return 'screenshot saved', 200
+#@app.route('/api/save_frame', methods=['POST'])
+#def save_frame():
+#  resp = request.get_json()
+#  
+#  data = resp['screenshot'].split(',')[1]
+#  w = resp['width']
+#  h = resp['height']
+#  key = resp['key']
+#  count = resp['count']
+#  im = Image.open(BytesIO(base64.b64decode(data)))
+#  im = im.crop((0,0,w,h))
+#
+#  path = os.path.join(os.getcwd(), f'images/{key}_{count}.png')
+#  print(path)
+#  im.save(path)
+#  return 'screenshot saved', 200
 
 if __name__ == "__main__":
   app.run()
